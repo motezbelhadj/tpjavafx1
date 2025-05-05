1b5feeb948581656d3e0ad8a5bcb8d653243f8a6
motezbelhadj <edumotez@gmail.com>
2025-05-05 10:24:24 +0100

    Your message

---
+++ TPJFX-master/src/main/java/com/example/PremiereFenetre.java
@@ -0,0 +1,35 @@
+/*package com.example;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+
+public class PremiereFenetre extends Application{
+    @Override
+ public void start(Stage primaryStage) {
+ Group root = new Group();
+ Scene scene = new Scene(root, 500, 450, Color.GREEN);
+ ObservableList<Node> liste = root.getChildren();
+ Line ligne = new Line(100, 100, 200, 250);
+ Circle cercle = new Circle(300, 300, 50);
+Rectangle rectangle = new Rectangle(200, 200, 100, 50);
+ liste.add(ligne);
+ liste.add(cercle);
+ liste.add(rectangle);
+ primaryStage.setTitle("Ma première fenêtre");
+ primaryStage.setScene(scene);
+ primaryStage.show();
+ }
+ public static void main(String[] args) {
+ launch(args);
+ }
+
+}*/
